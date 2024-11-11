ef5f3a75d004e2149842bd246758046f8d36d595
SYH <car0628@naver.com>
2024-11-12 06:10:18 +0900

    프로젝트 시작

---
+++ src/main/java/hp_price/controller/MainController.java
@@ -0,0 +1,38 @@
+package hp_price.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+    @ResponseBody
+    @RequestMapping("/")
+    public String getHtml() {
+        try {
+
+            String url = "https://gall.dcinside.com/mgallery/board/lists/?id=newheadphone&sort_type=N&search_head=120&page=1";
+            Document doc = Jsoup.connect(url).get();
+            Elements el = doc.select("section div ul li");
+            System.out.println(el);
+
+
+            return el.toString();
+        }catch(IOException e) {
+            e.printStackTrace();
+            return "1";
+        }
+
+    }
+
+}
