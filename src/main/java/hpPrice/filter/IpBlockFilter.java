ef46097a40b53fe478e88ff8523060d49764a300
SYH <car0628@naver.com>
2025-02-16 13:09:35 +0900

    특정 IP 차단 기능 추가 (일부 기능 허용)

--- src/main/java/hpPrice/filter/IpBlockFilter.java
+++ src/main/java/hpPrice/filter/IpBlockFilter.java
@@ -6,15 +6,26 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static hpPrice.common.ipBlock.IpAddressUtil.*;
 
 @Slf4j
 @Component
 public class IpBlockFilter extends OncePerRequestFilter {
+
+    private final List<String> allowedPaths =
+            Arrays.asList("/css/**", "/js/**", "/error/**", "/favicon/**", "/feedback");
+
+    private final Set<String> blockedIps = new HashSet<>
+            (Arrays.asList("45.148.10.90", "195.178.110.163", "118.235.80.48"));
 
 
     @Override
@@ -30,9 +41,14 @@ public class IpBlockFilter extends OncePerRequestFilter {
         String clientIp = getClientIp(request);
         if (blockedIps.contains(clientIp)) {
             response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied");
-            log.info("차단된 IP 접근 시도 -> " + clientIp);
+            log.info("차단된 IP 접근 시도 -> {}", clientIp);
             return;
         }
         filterChain.doFilter(request, response);
+    }
+
+    private boolean isAllowedPath(HttpServletRequest request) {
+        return allowedPaths.stream().anyMatch(path ->
+                new AntPathMatcher().match(path, request.getRequestURI()));
     }
 }
