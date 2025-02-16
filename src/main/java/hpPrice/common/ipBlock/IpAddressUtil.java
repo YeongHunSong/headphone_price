745bde6cef944efe011de5a25eaa10e268faa09e
SYH <car0628@naver.com>
2025-02-16 13:02:01 +0900

    특정 IP 차단 기능 추가

--- src/main/java/hpPrice/common/ipBlock/IpAddressUtil.java
+++ src/main/java/hpPrice/common/ipBlock/IpAddressUtil.java
@@ -1,8 +1,16 @@
 package hpPrice.common.ipBlock;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.util.AntPathMatcher;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class IpAddressUtil {
+    private IpAddressUtil() {}
+
     public static String getClientIp(HttpServletRequest request) {
         String xForwardedFor = request.getHeader("X-Forwarded-For");
         if (xForwardedFor != null && !xForwardedFor.isEmpty()) {
@@ -10,4 +18,24 @@ public class IpAddressUtil {
         }
         return request.getRemoteAddr();
     }
+
+    public static boolean isAllowedPath(HttpServletRequest request) {
+        return allowedPaths.stream().anyMatch(path ->
+                new AntPathMatcher().match(path, request.getRequestURI()));
+    }
+
+
+
+
+
+
+
+
+
+
+    public static final Set<String> blockedIps = new HashSet<>
+            (Arrays.asList("45.148.10.90", "195.178.110.163", "118.235.80.48"));
+
+    public static final List<String> allowedPaths =
+            Arrays.asList("/css/**", "/js/**", "/error/**", "/favicon/**", "/feedback");
 }
--- src/main/java/hpPrice/filter/IpBlockFilter.java
+++ src/main/java/hpPrice/filter/IpBlockFilter.java
@@ -1,6 +1,5 @@
 package hpPrice.filter;
 
-import hpPrice.common.ipBlock.IpAddressUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,21 +9,25 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+
+import static hpPrice.common.ipBlock.IpAddressUtil.*;
 
 @Slf4j
 @Component
 public class IpBlockFilter extends OncePerRequestFilter {
 
-    private final Set<String> blockedIps = new HashSet<>
-            (Arrays.asList("45.148.10.90", "195.178.110.163", "118.235.80.48"));
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        String clientIp = IpAddressUtil.getClientIp(request);
+
+        // 허용된 경로인지 확인
+        if (isAllowedPath(request)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String clientIp = getClientIp(request);
         if (blockedIps.contains(clientIp)) {
             response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied");
             log.info("차단된 IP 접근 시도 -> " + clientIp);
