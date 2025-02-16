c938bf03fe4973cb168e601bae2dc3c39697cbae
SYH <car0628@naver.com>
2025-02-16 11:18:53 +0900

    favicon 설정

--- src/main/java/hpPrice/config/SecurityConfig.java
+++ src/main/java/hpPrice/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package hpPrice.config;
 
 
+import jakarta.servlet.Filter;
+import org.apache.catalina.filters.RateLimitFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+
 
 @Configuration
 @EnableWebSecurity
@@ -17,27 +22,43 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers(HttpMethod.GET, "/").permitAll()           // /            GET 허용
-                        .requestMatchers(HttpMethod.GET, "/dcsff/**").permitAll()   // /dcsff       GET 허용
                         .requestMatchers(HttpMethod.GET, "/drhp/**").permitAll()    // /drhp        GET 허용
+                        .requestMatchers(HttpMethod.GET, "/dcsff/**").permitAll()   // /dcsff       GET 허용
                         .requestMatchers(HttpMethod.GET, "/feedback").permitAll()   // /feedback    GET 허용
                         .requestMatchers(HttpMethod.POST, "/feedback").permitAll()  // /feedback    POST 허용
 
                         .requestMatchers(HttpMethod.GET, "/js/**").permitAll() //       JS 허용
                         .requestMatchers(HttpMethod.GET, "/css/**").permitAll() //      CSS 허용
-                        .requestMatchers(HttpMethod.GET, "/static/**").permitAll() //   CSS JS 접근 허용
-                        .requestMatchers(HttpMethod.GET, "/error/**").permitAll() //    에러페이지 허용
-                        .anyRequest().denyAll() // 그 외 전부 차단
+                        .requestMatchers(HttpMethod.GET, "/favicon/**").permitAll() // favicon 허용
+                        .requestMatchers(HttpMethod.GET, "/favicon.ico").permitAll() // favicon 허용
+                        .requestMatchers(HttpMethod.GET, "/templates/error/**").permitAll() //    에러페이지 허용
+                        .anyRequest().denyAll()) // 그 외 전부 차단
+
+                .exceptionHandling(exceptions -> exceptions
+                        .accessDeniedPage("/error/403")
                 );
 
         http
                 .headers(headers -> headers
+                        .referrerPolicy(referrer -> referrer
+                                .policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN))
+
                         .contentSecurityPolicy(csp -> csp
                                 .policyDirectives("default-src 'self';" +
                                         "img-src 'self' data: https://dcimg1.dcinside.co.kr https://dcimg3.dcinside.co.kr " +
                                         "https://dcimg4.dcinside.co.kr https://dcimg1.dcinside.com " +
                                         "https://cafe.pstatic.net ;" +
                                         "style-src 'self' 'unsafe-inline';" +
-                                        "script-src 'self' 'unsafe-inline';")));
+                                        "script-src 'self' 'unsafe-inline';"))
+
+                        .permissionsPolicy(permissions -> permissions
+                                .policy("display-capture=(self), picture-in-picture=(self), " +
+                                        "accelerometer=(), autoplay=(), camera=(), document-domain=(), encrypted-media=(), " +
+                                        "fullscreen=(), geolocation=(), gyroscope=(), magnetometer=(), " +
+                                        "microphone=(), midi=(), payment=(), publickey-credentials-get=(), " +
+                                        "screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()")
+                        )
+                );
 
 //        http
 //                .requiresChannel(channel -> channel
@@ -45,5 +66,13 @@ public class SecurityConfig {
 //                );
 
         return http.build();
+    }
+
+    @Bean
+    public FilterRegistrationBean<Filter> rateLimitFilter() {
+        FilterRegistrationBean<Filter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RateLimitFilter());
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
     }
 }
--- src/main/java/hpPrice/config/WebConfig.java
+++ src/main/java/hpPrice/config/WebConfig.java
@@ -2,15 +2,9 @@ package hpPrice.config;
 
 import hpPrice.interceptor.AsyncVisitorInterceptor;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -23,32 +17,37 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(asyncVisitorInterceptor)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/css/**", "/js/**");
+                .excludePathPatterns("/css/**", "/js/**", "/favicon/**");
     }
 
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
-        return tomcat;
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/favicon.ico")
+                .addResourceLocations("classpath:/static/favicon/");
     }
 
-    private Connector redirectConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(80);
-        connector.setSecure(false);
-        connector.setRedirectPort(443);
-        return connector;
-    }
+//    @Bean
+//    public ServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint securityConstraint = new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+//        return tomcat;
+//    }
+//
+//    private Connector redirectConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        connector.setPort(80);
+//        connector.setSecure(false);
+//        connector.setRedirectPort(443);
+//        return connector;
+//    }
 }
