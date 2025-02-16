4b49802fb70f552479170077eecae6292674c671
SYH <car0628@naver.com>
2025-02-16 13:15:02 +0900

    특정 IP 차단 기능 추가 (일부 기능 허용)

--- src/main/java/hpPrice/filter/IpBlockFilter.java
+++ src/main/java/hpPrice/filter/IpBlockFilter.java
@@ -25,7 +25,7 @@ public class IpBlockFilter extends OncePerRequestFilter {
             Arrays.asList("/css/**", "/js/**", "/error/**", "/favicon/**", "/feedback");
 
     private final Set<String> blockedIps = new HashSet<>
-            (Arrays.asList("45.148.10.90", "195.178.110.163", "118.235.80.48"));
+            (Arrays.asList("45.148.10.90", "195.178.110.163"));
 
 
     @Override
